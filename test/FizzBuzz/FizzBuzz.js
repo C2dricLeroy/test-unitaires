5539571f307a5cd711f9bc1d4e4e52f7eea6c632
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-02-08 15:20:12 +0100

    create a fizzbuzz test

---
+++ test/FizzBuzz/FizzBuzz.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  FizzBuzz : function(i) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    return 'FizzBuzz';
+  } if (i % 3 === 0) {
+    return 'Fizz';
+  } if (i % 5 === 0) {
+    return 'Buzz';
+  } else return i;
+  }
+
+}
+
+
+
+
